5b0c91b87ea59de01e375ca5bd5f7cf7e4212753
Daniel Holden <theonlydancingbanana@hotmail.com>
2013-04-14 16:11:21 -0400

    Production merge

--- cello.py
+++ cello.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask import render_template
 from flask import Markup
 
+import os
 import markdown
 
 app = Flask(__name__)
+app.root_path = os.path.dirname(__file__)
 
 @app.route('/')
 @app.route('/<page>')
@@ -12,20 +14,20 @@ app = Flask(__name__)
 def index(page="home", section=None):
     
     if not (page in ["home", "documentation"]): page = "home"
-    
-    if (page    in ["documentation"] and 
+    if (page     in ["documentation"] and 
         section in ["functions", "types", "memory", "exceptions"]):
         section = "_"+section
     else:
         section = ""
-    
-    content = open("pages/"+page+section+".md").read()
-        
+                                                
+    content = open(os.path.join(app.root_path, "pages", page + section+".md")).read()
     content = markdown.markdown(content)
     content = content.replace("<pre><code>", "<pre><code data-language=\"libcello\">")
     content = content.replace("</h2>", "</h2><hr/>")
     content = Markup(content)
-    
+                                                                       
     return render_template('page.html', content=content)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
+
